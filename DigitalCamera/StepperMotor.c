bfec702767af8bb27cd283aef6915e41a6c8ef95
na5h09 <42976853+na5h09@users.noreply.github.com>
2021-11-02 11:45:48 -0500

    Create StepperMotor.c

---
+++ DigitalCamera/StepperMotor.c
@@ -0,0 +1,56 @@
+#include "SysTick.h"
+#include "TExaS.h"
+#include <stdint.h>
+#include "../inc/tm4c123gh6pm.h"
+
+volatile int delay;
+
+	uint8_t current = 0; //temp variables
+	uint8_t input_t = 0;
+
+void EnableInterrupts(void);
+// edit the following only if you need to move pins from PA4, PE3-0      
+// logic analyzer on the real board
+#define PA4       (*((volatile unsigned long *)0x40004040))
+#define PE50      (*((volatile unsigned long *)0x400240FC))
+void SendDataToLogicAnalyzer(void){
+  UART0_DR_R = 0x80|(PA4<<2)|PE50;
+}
+
+int Stepper_Check(void){ 
+  //TExaS_Init(&SendDataToLogicAnalyzer);    // activate logic analyzer and set system clock to 80 MHz
+  SysTick_Init();   
+// you initialize your system here
+	SYSCTL_RCGCGPIO_R |= 0x33; //initialize each port used
+	delay++;
+	
+	GPIO_PORTE_DIR_R |= 0x1F;
+	GPIO_PORTE_DEN_R |= 0x1F;
+	
+	GPIO_PORTA_DIR_R &= 0xCF;
+	GPIO_PORTA_DEN_R |= 0x30;
+	
+	GPIO_PORTB_DIR_R |= 0x40;
+	GPIO_PORTB_DEN_R |= 0x40;
+	
+	GPIO_PORTF_DIR_R |= 0x02;
+	GPIO_PORTF_DEN_R |= 0x02;
+	
+	
+	EnableInterrupts();   
+  while(1){
+// output
+// wait
+// input
+// next		
+		GPIO_PORTF_DATA_R ^= 0x02; //toggle the heartbeat LED
+		
+		GPIO_PORTE_DATA_R = fsm[current].Out; //output to the motor
+		//GPIO_PORTB_DATA_R = fsm[current].LED; //toggle the light when it is on
+		
+		//SysTick_Wait10ms(FSM[current].delay); //delay for how ever long each state runs for
+		
+		input_t = (GPIO_PORTA_DATA_R)/16;		//take the inputs and shift them right
+		current = fsm[current].Next[input_t]; //use the new value as the index of the next state graph
+  }
+}
