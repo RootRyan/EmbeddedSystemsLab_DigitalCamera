a075180b70fbf5a6a31ee3c1a224762f92270184
Jacob Dach <60268513+jacobdach@users.noreply.github.com>
2021-11-02 14:28:47 -0500

    Update menus.h

--- DigitalCamera/menus.h
+++ DigitalCamera/menus.h
@@ -1,9 +1,12 @@
 #include <stdint.h>
 
 struct Menu{
+	//keeps track of current state
 		int cur;
+	// array of all menus and sub menus 
 		char** menu;
-		char* vals;
+	//current selected value for each menu, refers to index in sub meny
+		int* vals;
 } mainmenu;
 
 struct Menu Menu_Init(void);
